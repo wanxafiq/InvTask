57dc4b09cdf843cce22e1dc3d70cd042eb8b5d6c
Wan Afiq <wanafiqkz97@gmail.com>
2023-01-25 21:56:32 +0800

    First commit.

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!-- Created by Wan Afiq -->
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <!-- <script src=""></script> -->
+    </head>
+    <body>
+        <img class="wave" src="img/wave.png">
+        <div class="container">
+            <div class="img">
+                <img src="img/img.svg">
+            </div>
+            <div class="login-container">
+                <form action=
+    </body>
+    
+</html>
